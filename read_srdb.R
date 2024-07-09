8267ecfcb62905a44ccf1ebc7a47a62ce50efaa4
Erin-Morrison <erin.t.morrison@pnnl.gov>
2024-07-09 12:09:26 -0700

    Trying to make a map plot

--- read_srdb.R
+++ read_srdb.R
@@ -1,5 +1,7 @@
+#Read in soil respiration data
 srdb <- read.csv("srdb-data.csv")
 
+#Filters for only high latitude data and chooses columns to focus on
 library(dplyr)
 srdb_hl <- srdb %>%
   filter(Latitude > 50) %>%
@@ -8,4 +10,11 @@ srdb_hl <- srdb %>%
 # Sanity check - quickly plot the data colored by country
 library(ggplot2)
 p <- ggplot(srdb_hl, aes(Longitude, Latitude, color = Country)) + geom_point()
-print(p)
+
+# Improved plot on map (not working)
+library(maps)
+m <- ggplot(map_data("world"), aes(x=long, y=lat, group=group)) + geom_polygon()
+mapData <- map_data("world") %>% rename("Latitude" = "lat")
+mapData <- srdb %>% left_join(mapData, by = "Latitude")
+a <- ggplot(mapData, aes(x=long, y=Latitude, group=group)) + geom_polygon()
+print(a)

d34764da3f9cb85944e27cc9569cd35dda7a3e55
Erin-Morrison <erin.t.morrison@pnnl.gov>
2024-07-11 12:00:41 -0700

    Circumpolar map and MAT plots

--- read_srdb.R
+++ read_srdb.R
@@ -3,10 +3,13 @@ srdb <- read.csv("srdb-data.csv")
 
 #Filters for only high latitude data and chooses columns to focus on
 library(dplyr)
-srdb_hl <- srdb %>%
+srdb_hlf <- srdb %>%
   #ask about filter this gives pretty much only European data
-  filter(Latitude > 50 & Quality_flag != "Q13" & Quality_flag != "Q12" & Quality_flag != "Q11" & Manipulation != "None" & !is.na(Rs_annual)) %>%
+  filter(Latitude >= 50 & Quality_flag != "Q13" & Quality_flag != "Q12" & Quality_flag != "Q11" & Manipulation != "None" & !is.na(Rs_annual)) %>%
   select(Country, Latitude, Longitude, Rs_annual)
+srdb_hl <- srdb %>% 
+  filter(Latitude >= 50) %>%
+  select(Country, Latitude, Longitude, Rs_annual, MAT, Rs_growingseason)
 
 # Sanity check - quickly plot the data colored by country
 library(ggplot2)
@@ -19,13 +22,16 @@ map(database = "world")
 points(x=srdb_hl$Longitude, y=srdb_hl$Latitude, col="Purple")
 
 #2
-m2 <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + geom_point(data = srdb_hl, mapping = aes(Longitude, Latitude, color = Country))
+m2 <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + 
+  geom_point(data = srdb_hl, mapping = aes(Longitude, Latitude, color = Country))
 
-#3 (doesn't work at all)
-#m <- ggplot(map_data("world"), aes(x=long, y=lat, group=group)) + geom_polygon()
-#print(m)
-#mapData <- map_data("world") %>% rename(c("Latitude" = "lat", "Longitude" = "long"))
-#mapData <- left_join(mapData, srdb_hl, by = c("Latitude", "Longitude")) %>% filter(!is.na(mapData$Rs_annual))
-#View(mapData)
-#a <- ggplot(mapData, aes(x=long, y=Latitude, group=group)) + geom_polygon()
-#print(a)
+#3 circumpolar
+library(mapproj)
+m3 <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + 
+  coord_map("ortho") + 
+  scale_y_continuous(breaks = seq(30, 90, by = 10), labels = NULL) +
+  geom_point(srdb_hl, mapping = aes(Longitude, Latitude, color = Country, size = Rs_annual))
+
+#MAP column
+MATvRSA <- ggplot(srdb_hl, aes(MAT, Rs_annual)) + geom_point() + geom_smooth()
+MATvRSG <- ggplot(srdb_hl, aes(MAT, Rs_growingseason)) + geom_point() + geom_smooth()

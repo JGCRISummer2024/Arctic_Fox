0964cccede0ef4e819a2774e80cb953bdf49caab
Erin-Morrison <erin.t.morrison@pnnl.gov>
2024-08-14 14:36:24 -0400

    Combine ancillary data code

--- read_srdb.R
+++ read_srdb.R
@@ -7,7 +7,6 @@ library(car)
 library(caret)
 
 #### Get data ####
-
 #Soil Respiration Data Base
 srdb <- read.csv("srdb-data.csv")
 srdb <- srdb %>% filter(!is.na(Longitude) & !is.na(Latitude))
@@ -92,6 +91,97 @@ RsMap <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat,
   labs(color = "Annual Rs (g C m^-2)")
 
 #### Ancillary Data Maps & Graphs ####
+##MODIS
+#World map
+modisMap <- ggplot() + 
+  geom_polygon(map_data("world"), mapping = aes(long, lat, group=group)) + 
+  geom_point(srdb, mapping=aes(Longitude, Latitude, color=modis)) +
+  scale_color_gradient(low = "white", high = "darkgreen")
+#Built in ANPP vs MODIS
+modisCheck <- ggplot(srdb, aes(ANPP, modis)) + 
+  geom_point() + 
+  geom_point(srdb_hl, mapping = aes(ANPP, modis), color = "cyan") +
+  geom_abline() +
+  xlab("SRDB ANPP") +
+  ylab("MODIS NPP") +
+  ggtitle("On Site ANPP vs MODIS")
+modisCheckhl <- ggplot(srdb_hl, aes(ANPP, modis)) + geom_point() + geom_abline()
+#Resolution 0.5 vs 0.1
+modis.5 <- terra::rast("MOD17A3H_Y_NPP_2023-01-01_rgb_720x360.TIFF")
+modis_clamp.5 <- terra::clamp(modis.5, lower=1, upper=254, values=FALSE)
+srdb_modis.5 <- terra::extract(modis_clamp.5, srdb[15:14])
+srdb$modis.5 <- srdb_modis.5$`MOD17A3H_Y_NPP_2023-01-01_rgb_720x360` * (1950/254) + 50 #scale data
+resolution <- ggplot(srdb, aes(modis, modis.5)) + geom_point() + geom_abline()
+
+##Permafrost
+#World map
+permafrostMap <- ggplot() + 
+  geom_polygon(map_data("world"), mapping = aes(long, lat, group=group)) + 
+  geom_point(srdb, mapping=aes(Longitude, Latitude, color=permafrost)) +
+  scale_color_gradient(low = "pink", high = "blue")
+
+##Peatlands
+#maps world and circumpolar
+peatMap <- ggplot() + 
+  geom_polygon(map_data("world"), mapping = aes(long, lat, group=group)) + 
+  geom_point(srdb, mapping=aes(Longitude, Latitude, color=peatland)) +
+  scale_color_gradient(low = "blue", high = "yellow")
+peatMaphl <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + 
+  coord_map("ortho") + 
+  scale_y_continuous(breaks = seq(30, 90, by = 10), labels = NULL) +
+  geom_point(data = srdb_hl, mapping = aes(Longitude, Latitude, color = peatland)) +
+  scale_color_gradient(low="blue", high="yellow")
+
+#WorldClim Temperature
+#maps world and circumpolar
+wTM <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + 
+  geom_point(data = srdb, mapping = aes(Longitude, Latitude, color = MAT_wc)) + 
+  scale_color_gradient(low=c("purple", "blue", "cyan", "green"), high=c("yellow", "orange", "red"))
+hlTM <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + 
+  coord_map("ortho") + 
+  scale_y_continuous(breaks = seq(30, 90, by = 10), labels = NULL) +
+  xlab("") +
+  ylab("")+
+  geom_point(data = srdb_hl, mapping = aes(Longitude, Latitude, color = MAT_wc), size = 1) +
+  scale_color_gradient(low=c("purple", "blue", "cyan", "green"), high=c("yellow", "orange", "red")) +
+  ggtitle("Mean Annual Temperature") +
+  theme(panel.grid.major = element_line(linewidth = 0.25, linetype = 'dashed', color = "darkgrey"), axis.ticks=element_blank()) +
+  labs(color = "Degrees Celsius")
+#Graph MAT by MAT_wc
+tempCheck <- ggplot(srdb, aes(MAT, MAT_wc)) + 
+  geom_point() + 
+  geom_point(data = srdb_hl, mapping = aes(MAT, MAT_wc), color="cyan") + 
+  geom_abline() +
+  xlab("Mean Annual Temperature SRDB") +
+  ylab("Mean Annual Temperature WorldClim") +
+  ggtitle("SRDB Temperature vs WorldClim Temperature")
+tempCheckhl <- ggplot(srdb_hl, aes(MAT, MAT_wc)) + geom_point() + geom_abline()
+
+##WorldClim Precipitation
+#maps world and circumpolar
+wPM <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + 
+  geom_point(data = srdb, mapping = aes(Longitude, Latitude, color = MAP_wc)) + 
+  scale_color_gradientn(colors = rainbow(5))
+hlPM <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + 
+  coord_map("ortho") + 
+  scale_y_continuous(breaks = seq(30, 90, by = 10), labels = NULL) +
+  geom_point(data = srdb_hl, mapping = aes(Longitude, Latitude, color = MAP_wc)) + 
+  scale_color_gradientn(colors = rainbow(5))
+#Graph MAP by MAP_wc
+percCheck <- ggplot(srdb, aes(MAP, MAP_wc)) + geom_point() + geom_abline()
+percCheckhl <- ggplot(srdb_hl, aes(MAP, MAP_wc)) + geom_point() + geom_abline()
+
+##WorldClim Solar Radiation
+sradMap <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + 
+  geom_point(data = srdb, mapping = aes(Longitude, Latitude, color = srad)) + 
+  scale_color_gradient(low=c("purple", "blue", "cyan", "green"), high=c("yellow", "orange", "red"))
+
+##SoilGrids
+OCSmap <- ggplot() + geom_polygon(map_data("world"), mapping = aes(x=long, y=lat, group=group)) + 
+  coord_map("ortho") + 
+  scale_y_continuous(breaks = seq(30, 90, by = 10), labels = NULL) +
+  geom_point(data = srdb_hl, mapping = aes(Longitude, Latitude, color = OCS)) + 
+  scale_color_gradientn(colors = rainbow(5))
 
 #### Linear Regression Models ####
 
